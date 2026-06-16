bf386aa8a01331d45080eef4756c24d89d4c310f
gabriel-aalmeida <gabrieldealmeidaalfredo@gmail.com>
2026-06-16 15:41:31 -0300

    Declara prototipos das funcoes e cria funcao main vazia
    Declara prototipos das funcoes e cria funcao main vazia

--- quiz.c
+++ quiz.c
@@ -11,3 +11,17 @@
 
 #define TOTAL_PERGUNTAS 8
 #define PONTOS_POR_ACERTO 10
+
+void exibirBoasVindas();
+void exibirResultado(int pontos, int acertos);
+int fazerPergunta(int numero, const char *pergunta,
+                  const char *a, const char *b, const char *c, const char *d,
+                  char respostaCorreta);
+
+int main() {
+    int pontos  = 0;
+    int acertos = 0;
+    char resposta;
+
+    return 0;
+}
