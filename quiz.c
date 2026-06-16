1d078499e5ca69db871f26e1cc13f88ff0d88d13
gabriel-aalmeida <gabrieldealmeidaalfredo@gmail.com>
2026-06-16 15:07:47 -0300

    Create quiz.c
    Adiciona cabecalho e diretivas de definicao iniciais

---
+++ quiz.c
@@ -0,0 +1,13 @@
+/*
+ * =====================================================
+ *  QUIZ DA COPA DO MUNDO - Escape Room: Estadio Secreto
+ *  Atividade Final - Gamificacao
+ *  Disciplina: Introducao a Programacao
+ *  Aluno: Gabriel de Almeida Alfredo
+ * =====================================================
+ */
+
+#include <stdio.h>
+
+#define TOTAL_PERGUNTAS 8
+#define PONTOS_POR_ACERTO 10
